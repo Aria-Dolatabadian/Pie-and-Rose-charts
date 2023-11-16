8747c59c50e99fadcb654a89f2661f3dd6962af2
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-16 15:56:45 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -3,21 +3,21 @@ library(cowplot)
 
 df <- data.frame(
   variable = rep(c("cat", "mouse", "dog", "bird", "fly")),
-  value = c(10, 20, 30, 40, 80)
+  value = c(10, 20, 30, 30, 10)
 )
 
 # Create pie chart
 pie_chart <- ggplot(df, aes(x = "", y = value, fill = variable)) +
   geom_bar(width = 1, stat = "identity", color = "black") +  # Add black border
   coord_polar("y", start = pi / 3) +
-  scale_y_continuous(breaks = seq(0, 80, by = 10)) +  # Adjust tick marks
+  scale_y_continuous(breaks = seq(0, 90, by = 5)) +  # Adjust tick marks
   ggtitle("Pie Chart")
 
 # Create rose chart
 rose_chart <- ggplot(df, aes(x = variable, y = value, fill = variable)) +
   geom_bar(width = 1, stat = "identity", color = "black") +
   coord_polar("y", start = pi / 3) +
-  scale_y_continuous(breaks = seq(0, 80, by = 10)) +
+  scale_y_continuous(breaks = seq(0, 80, by = 5)) +
   ggtitle("Rose Chart")
 
 # Arrange the two charts side by side
